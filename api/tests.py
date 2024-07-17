6cabbe4a85522aec7b30b8597e13ce096a04ec81
RAJARATNAM PAVITHARAN <para005@ess.ais.ac.nz>
2024-07-18 11:23:42 +1200

    GitHub Actions Testing Done gor user registration.

--- api/tests.py
+++ api/tests.py
@@ -4,15 +4,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
-from django.core.management.commands.test import Command as BaseCommand
 
-
-class Command(BaseCommand):
-    def handle(self, *test_labels, **options):
-        # Wrap Django's built-in test command to always delete the database if
-        # it exists
-        options["interactive"] = False
-        return super().handle(*test_labels, **options)
 class CreateUserViewTests(APITestCase):
     def setUp(self):
         self.url = reverse('register')  # Ensure this name matches your URL pattern name for CreateUserView
